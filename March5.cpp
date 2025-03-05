64648139467c8dff315a9635e82163c1bb238931
Sahil-dev-2005 <sahilbrokvs2005@gmail.com>
2025-03-05 16:50:56 +0530

    added code for Search in rotated sorted array-2

--- March5.cpp
+++ March5.cpp
@@ -34,3 +34,42 @@ public:
         return -1;
     }
 };
+
+
+// Search element in rotated sorted array-2
+// With duplicates
+// an edge case should be handled where we can't figure out which half is sorted or not
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int x) {
+        int n = nums.size();
+        int low=0, high=n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]==x) return true;
+            
+            //EDGE CASE
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            
+            if(nums[mid]>=nums[low]){
+                if(x>=nums[low] && x<=nums[mid]){
+                    high=mid-1;
+                } else {
+                    low=mid+1;
+                }
+            } else {
+                if(x>=nums[mid] && x<=nums[high]){
+                    low=mid+1;
+                } else {
+                    high=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+};
