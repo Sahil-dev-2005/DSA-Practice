b2b7ef69071300218fa19681c8059ff45160c75e
Sahil-dev-2005 <sahilbrokvs2005@gmail.com>
2025-03-05 16:23:06 +0530

    Search in rotated sorted array-1

---
+++ March5.cpp
@@ -0,0 +1,36 @@
+// key concept of binary search
+// IDENTIFY THE HALF THAT YOU ARE SURE TO ELIMINATE
+
+// Search element in rotated sorted array-1
+// 2 STEPS
+// 1. Identify the sorted half.
+// 2. Check if target can lie in that half. If not then go to the unsorted half.
+
+class Solution {
+public:
+    int search(vector<int>& nums, int x) {
+        int n = nums.size();
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]==x) return mid;
+            //if left half is sorted
+            if(nums[mid]>=nums[low]){
+                if(x>=nums[low] && x<=nums[mid]){
+                    high = mid-1;
+                } else {
+                    low = mid+1;
+                }
+            }
+            //if right half is sorted
+            else{
+                if(x>=nums[mid] && x<=nums[high]){
+                    low = mid+1;
+                } else {
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
